1d21a57c470d3ab656a787119b622c60a404c02d
nmorduch <1051450+nmorduch@users.noreply.github.com>
2025-01-31 15:24:34 +0000

    Replace default content with basic table and header

---
+++ src/components/Country.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  countryName: {
+    type: String,
+    required: true,
+  },
+  region: {
+    type: String,
+    required: true,
+  },
+  totalEnergySupply: {
+    type: [Number, String],
+    default: '—',
+  },
+  IEAMember: {
+    type: Boolean,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <tr>
+    <th>{{ countryName }}</th>
+    <td>{{ region }}</td>
+    <td>{{ totalEnergySupply }}</td>
+    <td>{{ IEAMember }}</td>
+  </tr>
+</template>
